c50c4b935cade4e1861590527db6d11db3cd9a50
Rohan Molia <rohanmolia111213@gmail.com>
2020-07-05 22:58:56 +0530

    Components Part 1 Edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/menuComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            <img src={logo} className="App-logo" alt="logo"/>
-            React
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              <img src={logo} className="App-logo" alt="logo"/>
+              React
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
