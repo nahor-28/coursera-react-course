4e44738f5f4a3a749a24dae9fd8e9ef31e4d0013
Rohan Molia <rohanmolia111213@gmail.com>
2020-07-31 14:01:09 +0530

    Redux Actions

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -25,8 +25,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
         handleSubmit(values) {
             console.log('Current State is: ' + JSON.stringify(values));
-            alert('Current State is: ' + JSON.stringify(values));
-        }
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);        }
     
         render() {
             return(
@@ -49,9 +48,9 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                     </Col>
                                 </Row>
                                 <Row className="form-group">
-                                    <Label htmlFor="name" md={12}>Your Name</Label>
+                                    <Label htmlFor="author" md={12}>Your Name</Label>
                                     <Col md={12}>
-                                        <Control.text model=".name" id="name" name="name"
+                                        <Control.text model=".author" id="author" name="author"
                                             placeholder="Your Name"
                                             className="form-control"
                                             validators={{
@@ -60,7 +59,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                         />
                                         <Errors
                                             className="text-danger"
-                                            model=".name"
+                                            model=".author"
                                             show="touched"
                                             messages={{
                                                 required: 'Required',
@@ -117,7 +116,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
             );
     };
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
@@ -142,7 +141,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                 })
                         }
                     </ul>
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>               
             );
         }            
@@ -163,7 +162,9 @@ const DishDetail = (props) => {
                 </div>
                 <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments} 
+                         addComment={props.addComment}
+                         dishId={props.dish.id} />
                 </div>
             </div>
         );
